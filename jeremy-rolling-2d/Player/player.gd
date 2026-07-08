d5332d5425a53b433b574be2dda616fc2eeb3575
andro32 <168885773+andro-32@users.noreply.github.com>
2026-07-08 23:00:10 +1000

    this is the third time i've forgotten

--- jeremy-rolling-2d/Player/player.gd
+++ jeremy-rolling-2d/Player/player.gd
@@ -93,7 +93,7 @@ func _input(event: InputEvent) -> void:
 	
 		if event.is_action_pressed("menu"):
 			get_tree().change_scene_to_file("res://UI/menu.tscn")
-	if event is InputEventMouseMotion:
+	if event is InputEventScreenDrag:
 		get_node("Arrow").hide()
 		dragging = true
 		drag_pos = event.screen_relative
