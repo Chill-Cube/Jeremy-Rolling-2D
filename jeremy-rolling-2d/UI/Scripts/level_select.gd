4c4f4f091452fc3e89892cca66028ae0fe2e2d8a
Chill-Cube <212706581+Chill-Cube@users.noreply.github.com>
2026-07-21 21:15:42 +1000

    lobster count

--- jeremy-rolling-2d/UI/Scripts/level_select.gd
+++ jeremy-rolling-2d/UI/Scripts/level_select.gd
@@ -1,5 +1,6 @@
 extends Control
 var levels : Array
+var lobster_count := 0
 
 func _ready() -> void:
 	for level_name in LevelList.LEVELS:
@@ -13,10 +14,14 @@ func _ready() -> void:
 
 		var level_data = SaveManager.get_level_data(level_name)
 		var children := level.get_node("Lobster").get_children()
+		
+		lobster_count += level_data.collectables.count(true)
 
 		for i in children.size():
 			var ui: Sprite2D = children[i]
 			ui.modulate = ui.modulate if level_data.collectables[i] else Color(0.0, 0.0, 0.0, 0.384)
+	
+	%Counter.text = str(lobster_count)+"/"+str(LevelList.LEVELS.size() * 3)
 
 
 func _on_template_pressed() -> void:
